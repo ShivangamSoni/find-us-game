0cc813afdff5ea9c43359904ee8830ca2d6f789f
Shivangam Soni <shivangamsoni99@gmail.com>
2022-12-23 17:26:31 +0530

    Update: Game Layout

---
+++ src/Context/NoptificationContext.tsx
@@ -0,0 +1,34 @@
+// import type { ReactNode } from "react";
+// import { createContext, useState, useEffect, useContext } from "react";
+
+// import type { User } from "firebase/auth";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "../firebase";
+
+// const AuthContext = createContext({ open: false, text: "" });
+
+// interface Props {
+//     children: ReactNode;
+// }
+
+// export default function AuthProvider({ children }: Props) {
+//     const [isAuth, setIsAuth] = useState<User | null>(null);
+
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, (user) => {
+//             setIsAuth(user);
+//         });
+
+//         return () => unsubscribe();
+//     }, []);
+
+//     return (
+//         <AuthContext.Provider value={{ isAuth }}>
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// }
+
+// export const useAuthCtx = () => useContext(AuthContext);
+
+export default 10;
--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,6 +1,12 @@
-import { MouseEventHandler } from "react";
+import type { MouseEventHandler } from "react";
 
 import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Typography from "@mui/material/Typography";
+
+import Timer from "../../components/Timer/Timer";
 
 export default function Game() {
     const handleClick: MouseEventHandler<HTMLImageElement> = (e) => {
@@ -10,7 +16,65 @@ export default function Game() {
 
     return (
         <Box>
-            {/* TODO: ADD Game Header */}
+            <Box
+                component="header"
+                sx={{
+                    display: "flex",
+                    alignItems: {
+                        sm: "center",
+                    },
+                    justifyContent: "space-between",
+                    flexDirection: {
+                        xs: "column",
+                        sm: "row",
+                    },
+                    gap: 2,
+                }}
+            >
+                <Box sx={{ width: "100%" }}>
+                    <Typography variant="h5" component="h2">
+                        Find These Characters
+                    </Typography>
+                    <ImageList
+                        sx={{
+                            height: 200,
+                            gridTemplateColumns: {
+                                xs: "repeat(2, 1fr) !important",
+                                sm: "repeat(3, 1fr) !important",
+                                md: "repeat(4, 1fr) !important",
+                            },
+                        }}
+                    >
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <ImageListItem
+                                sx={{
+                                    "height": "inherit !important",
+                                    "& > img": {
+                                        objectFit: "contain !important",
+                                        height: "100% !important",
+                                    },
+                                }}
+                            >
+                                <img
+                                    src={require("../../Assets/smaple.jpg")}
+                                    alt=""
+                                    loading="lazy"
+                                />
+                                <ImageListItemBar
+                                    title="Mario"
+                                    subtitle="Not Found"
+                                    position="bottom"
+                                />
+                            </ImageListItem>
+                        ))}
+                    </ImageList>
+                </Box>
+
+                <Box>
+                    <Timer />
+                </Box>
+            </Box>
+
             <Box
                 sx={{
                     "& > img": {
