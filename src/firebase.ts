ee1b2bf6f16bf06384bf9c21d1713f36e4f06331
Shivangam Soni <shivangamsoni99@gmail.com>
2022-12-23 15:26:18 +0530

    Add: Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyChUz7R5rELHUB_YFVwGsKitGDFdGzwp64",
+    authDomain: "find-us-game.firebaseapp.com",
+    projectId: "find-us-game",
+    storageBucket: "find-us-game.appspot.com",
+    messagingSenderId: "89093524934",
+    appId: "1:89093524934:web:bb0a1002e6e6e8c900b6c7",
+    measurementId: "G-D9GLSL0KLG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get FirestoreDB Instance
+export const db = getFirestore(app);
+
+// Get Authentication Instance
+export const auth = getAuth(app);
+
+// Google Auth Provider
+export const googleAuthProvider = new GoogleAuthProvider();
