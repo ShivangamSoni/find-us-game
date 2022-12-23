1df27e6e539129d643edaa2b62e3b2dd0b563823
Shivangam Soni <shivangamsoni99@gmail.com>
2022-12-23 19:22:48 +0530

    Update: Timer Functionality

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,7 +1,65 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { keyframes } from "@mui/material/styles";
+
+import { useGameCtx } from "../../Context/GameContext";
+
+const clockTileStyle = {
+    display: "grid",
+    placeItems: "center",
+    padding: "1rem 1.2rem",
+    borderRadius: "0.3rem",
+    color: "#3e087d",
+    backgroundColor: "#f5e0ff",
+};
+
+const blink = keyframes`
+    to {
+        opacity: 0;
+    }
+`;
+
+const clockColonStyle = {
+    padding: 0,
+    fontWeight: "900",
+    fontSize: "2rem",
+    animation: `${blink} 1000ms infinite linear`,
+};
+
 export default function Timer() {
+    const {
+        time: { seconds, minutes },
+    } = useGameCtx();
+
     return (
-        <>
-            <h1>Timer</h1>
-        </>
+        <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+            }}
+        >
+            <Box sx={clockTileStyle}>
+                <Typography component="span" variant="h3">
+                    {minutes}
+                </Typography>
+                <Typography component="span" variant="subtitle2">
+                    Minutes
+                </Typography>
+            </Box>
+            <Box sx={clockColonStyle}>
+                <Typography component="span" variant="h4">
+                    :
+                </Typography>
+            </Box>
+            <Box sx={clockTileStyle}>
+                <Typography component="span" variant="h3">
+                    {seconds}
+                </Typography>
+                <Typography component="span" variant="subtitle2">
+                    Seconds
+                </Typography>
+            </Box>
+        </Box>
     );
 }
