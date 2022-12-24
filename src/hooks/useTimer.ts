326b6b220ce656b0d55155bc41ddaa589e0ff8d2
Shivangam Soni <shivangamsoni99@gmail.com>
2022-12-24 11:12:55 +0530

    Add: useTimer Custom Hook

---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useTimer() {
+    const [seconds, setSeconds] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const intervalId = useRef<NodeJS.Timer>();
+
+    useEffect(() => {
+        if (isRunning) {
+            intervalId.current = setInterval(() => {
+                setSeconds((prev) => prev + 1);
+            }, 1000);
+        } else {
+            clearInterval(intervalId.current);
+        }
+
+        return () => clearInterval(intervalId.current);
+    }, [isRunning]);
+
+    const time = {
+        minutes: Math.floor(seconds / 60),
+        seconds: Math.floor(seconds % 60),
+    };
+
+    const startTimer = useCallback(() => setIsRunning(true), [setIsRunning]);
+    const pauseTimer = useCallback(() => setIsRunning(false), [setIsRunning]);
+
+    const resetTimer = useCallback(() => {
+        setIsRunning(false);
+        setSeconds(0);
+    }, [setIsRunning, setSeconds]);
+
+    return { time, startTimer, pauseTimer, resetTimer };
+}
