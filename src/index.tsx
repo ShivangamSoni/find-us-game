5b191f088d73335dd591f9815e60f2a6d111caf4
Shivangam Soni <shivangamsoni99@gmail.com>
2022-12-27 19:16:17 +0530

    Update: Replace Font import with Font Link

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
-// MUI Fonts
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import CssBaseline from "@mui/material/CssBaseline";
 
 import AuthProvider from "./Context/AuthContext";
